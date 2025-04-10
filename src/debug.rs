0c823102a559e82a3378b6b7137c2dce90202900
temanmd <temacxz@gmail.com>
2025-04-10 20:56:34 +0500

    Remove useless debug system

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,3 @@
-use crate::player::Player;
 use bevy::prelude::*;
 use iyes_perf_ui::{PerfUiPlugin, prelude::PerfUiDefaultEntries};
 
@@ -7,7 +6,6 @@ pub struct DebugPlugin;
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_debug_ui);
-        app.add_systems(Update, print_position);
         app.add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default());
         app.add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin);
         app.add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin);
@@ -18,13 +16,4 @@ impl Plugin for DebugPlugin {
 
 fn add_debug_ui(mut commands: Commands) {
     commands.spawn(PerfUiDefaultEntries::default());
-}
-
-fn print_position(query: Query<(Entity, &Transform), With<Player>>) {
-    // for (entity, transform) in query.iter() {
-    //     info!(
-    //         "Entity {:?} is at position {:?},",
-    //         entity, transform.translation
-    //     );
-    // }
 }
